4f2a2f0d3250e13593eb3b9ce1dd7a89d28c3ee0
loginshin <95666408+loginshin@users.noreply.github.com>
2023-06-28 20:00:54 +0900

    Controller, View 분석
    Controller, View 분석

--- src/main/java/com/springmvc/controller/HomeController.java
+++ src/main/java/com/springmvc/controller/HomeController.java
@@ -11,28 +11,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
-@Controller
+@Controller //밑에 클래스가 컨트롤러 역할을 하는것을 나타냅니다.
 public class HomeController {
-	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	//리퀘스트매핑 어노테이션은 매개변수값과 같으면 해당 메서드가 작동한다.
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		model.addAttribute("serverTime", formattedDate ); //비즈니스 로직을 수행한 결과를 뷰에 보냅니다.
 		return "home";
 	}
 	
